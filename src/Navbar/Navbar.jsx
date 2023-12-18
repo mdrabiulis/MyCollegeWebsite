08da5061b0d023d34d115dfd83cb0ab3e67460cb
mdrabiulis <mdrabiulis155@gmail.com>
2023-12-19 03:11:35 +0600

    add button

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 import Logo from "./Logo/Logo";
+import './Navbar.css'
+import { FiAlignJustify, FiXCircle } from "react-icons/fi";
+
+
 
 
 const Navbar = () => {
+    const [click, setclick] = useState(false)
+
+    const hendelClick =()=>{
+        setclick(!click)
+    }
     return (
-        <div className="">
+        <div className="flex justify-between items-center bg-[#1b2430] py-5 px-20 shadow-xl">
             <Logo></Logo>
+            <nav>
+                <ul className="flex gap-8">
+                    <li className="text-[#fff] hover:text-[#17cf97] duration-300 ease-out font-bold hover:underline">Home</li>
+                    <li className="text-[#fff] hover:text-[#17cf97] duration-300 ease-out font-bold hover:underline">About</li>
+                    <li className="text-[#fff] hover:text-[#17cf97] duration-300 ease-out font-bold hover:underline">Blogs</li>
+                    <li className="text-[#fff] hover:text-[#17cf97] duration-300 ease-out font-bold hover:underline">Contact</li>
+                    <li className="text-[#fff] hover:text-[#17cf97] duration-300 ease-out font-bold hover:underline">Service</li>
+                </ul>
+            </nav>
+ 
+            
+<div className="" onClick={hendelClick}>
+{
+    click ? <button><FiXCircle className="text-white w-8 h-8"/></button> : <button><FiAlignJustify className="text-white w-8 h-8"/></button>
+}
+</div>
+
+
         </div>
     );
 };
